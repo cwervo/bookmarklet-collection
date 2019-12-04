017f4b687e87c839a425cdaca845137fb80849a1
Andrés Cuervo <acwervo@gmail.com>
2019-12-04 14:20:12 -0500

    Add clubhouse metadata, correct README dev instructions

--- clubhouse-gh-string/source.js
+++ clubhouse-gh-string/source.js
@@ -1,3 +1,5 @@
-// var clipEls = document.querySelectorAll('.clipboard'); clipEls[0].value = `[ch${clipEls[0].id.match(/\d+/)[0]}](${document.location.href})`
+// ==Bookmarklet==
+// @name Clubhouse Github parse string bookmarklet
+// @author Andres Cuervo
+// ==/Bookmarklet==
 var clipEl = document.querySelector('[id^=story-id]'); clipEl.value=`[ch${clipEl.id.match(/\d+/)[0]}](${document.location.href})`
-
